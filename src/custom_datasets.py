a807e97ddc3e0452586a8625fc07d543ca3b18c8
utkuozbulak <utku.ozbulak@gmail.com>
2017-09-14 22:36:00 +0900

    Comments

--- src/custom_datasets.py
+++ src/custom_datasets.py
@@ -47,15 +47,20 @@ class CustomDatasetFromCSV(Dataset):
 
     def __getitem__(self, index):
         single_image_label = self.labels[index]
+        # Create an empty numpy array to fill
         img_as_np = np.ones((28, 28), dtype='uint8')
+        # Fill the numpy array with data from pandas df
         for i in range(1, self.data.shape[1]):
             row_pos = (i-1) // self.height
             col_pos = (i-1) % self.width
             img_as_np[row_pos][col_pos] = self.data.iloc[index][i]
+        # Convert image from numpy array to PIL image, mode 'L' is for grayscale
         img_as_img = Image.fromarray(img_as_np)
         img_as_img = img_as_img.convert('L')
+        # Transform image to tensor
         if self.transform is not None:
             img_as_tensor = self.transform(img_as_img)
+        # Return image and the label
         return (img_as_tensor, single_image_label)
 
     def __len__(self):
